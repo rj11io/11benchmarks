16db09dce21dd02fdd661d1bd75b527d88def670
Ricardo Jorge <ricardojorgexyz@gmail.com>
2026-07-14 15:48:52 +0100

    fix: add analytics

--- v1/_app-boilerplate_/app/layout.tsx
+++ v1/_app-boilerplate_/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Geist_Mono, Inter } from "next/font/google"
+import { Analytics } from "@vercel/analytics/next"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -36,6 +37,7 @@ export default function RootLayout({
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
